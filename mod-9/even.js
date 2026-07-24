016acb52e1187090ea30c69f131c097fd8f49446
Robiul-Hosen <rhsojib612@gmail.com>
2026-07-25 03:54:03 +0600

    mod-9

---
+++ mod-9/even.js
@@ -0,0 +1,5 @@
+let number; 
+
+for (number = 0; number <= 10 ; number = number + 2){
+   console.log(number);
+}
---
+++ mod-9/sumALLnumber.js
@@ -0,0 +1,7 @@
+// sum of 1 to 10 ; 
+let i, j = 0;
+for (i=0 ; i <= 10; i++){
+    j = j+i;
+    console.log(i ," is ",j);
+}
+// console.log(j);
